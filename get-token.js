450fb6144f2dbf7afa0ed1aafff1e557cc4da18b
anafe03 <79051185+anafe03@users.noreply.github.com>
2025-06-24 10:49:51 -0700

    tokent changes

--- get-token.js
+++ get-token.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  'https://gdmdurzaeezcrgrmtabx.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdkbWR1cnphZWV6Y3Jncm10YWJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1OTk3MDUsImV4cCI6MjA2MzE3NTcwNX0.xbuXRr2jDAAG021blK5zeuqwO-5zMNq7_tEfW_oW7cQ'
+  'https://alyezazlyelroabhrdak.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFseWV6YXpseWVscm9hYmhyZGFrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MDc3MzUxNCwiZXhwIjoyMDY2MzQ5NTE0fQ.P7h6b5HqWM0wBzryak2oCp602OR42yHauZQ-brl7gLA'
 )
 
 const { data, error } = await supabase.auth.signInWithPassword({
